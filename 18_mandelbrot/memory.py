a2f291b545396cf47defef30185a20ad5f77e2a9
Bastian Löher <b.loeher@gsi.de>
2023-02-27 02:45:33 +0100

    Mandelbrot done, but something is broken.

--- 18_mandelbrot/memory.py
+++ 18_mandelbrot/memory.py
@@ -9,57 +9,113 @@ class Mem(Elaboratable):
         a.read("""begin:
 
         mandel_shift    equ 10
+        mandel_shift_m1 equ  9
         mandel_mul      equ 1024    ; (1 << mandel_shift)
         xmin            equ -2048   ; (-2 * mandel_mul)
         xmax            equ 2028    ; ( 2 * mandel_mul)
         ymin            equ -2048   ; (-2 * mandel_mul)
         ymax            equ 2028    ; ( 2 * mandel_mul)
-        dx              equ 136     ; (xmax - xmin) / 30
-        dy              equ 136     ; (ymax - ymin) / 30
+        dx              equ 51      ; (xmax - xmin) / 80
+        dy              equ 51      ; (ymax - ymin) / 80
         norm_max        equ 4096    ; (4 << mandel_shift)
+        io_leds         equ 4       ; (2 + 2)
+        slow_bit        equ 18      ; wait (1 << 18) clocks
 
+        LI      sp, 0x1800          ; end of RAM, 6 kB
         LI      gp, 0x400000        ; IO page
+
+        J       mandelstart
+
+        colormap:
+        DATAB   " ", ".", ",", ":"
+        DATAB   ";", "o", "x", "%"
+        DATAB   "#", "@", 0, 0
+
+        mandelstart:
+
+        LI      s0, 5               ; blink 5 times
+        blink:
+        LI      a0, 5
+        SW      a0, gp, io_leds
+        CALL    wait
+        LI      a0, 10
+        SW      a0, gp, io_leds
+        CALL    wait
+        ADDI    s0, s0, -1
+        BNEZ    s0, blink
+        LI      a0, 0
+        SW      a0, gp, io_leds
 
         LI      s1, 0
         LI      s3, xmin
         LI      s11, 80
-        LI      s10, norm_max
 
         loop_y:
         LI      s0, 0
         LI      s2, ymin
 
         loop_x:
-        MV      a0, s2
-        MV      a1, s2
-        CALL    mulsi3
-        SRLI    s4, a0, mandel_shift    ; s4 = x*x
-        MV      a0, s3
-        MV      a1, s3
-        CALL    mulsi3
-        SRLI    s5, a0, mandel_shift    ; s5 = y*y
-        ADD     s6, s4, s5              ; s6 = x*x + y*y
-        LI      a0, "*"
-        BLT     s6, s10, in_disk        ; if (x*x + y*y) < 4
-        LI      a0, " "
+        MV      s4, s2                  ; z <- c
+        MV      s5, s3
 
-        in_disk:
+        LI      s10, 9                  ; iter <- 9
+
+
+        loop_z:
+        MV      a0, s4
+        MV      a1, s4
+        CALL    mulsi3
+        SRLI    s6, a0, mandel_shift    ; Zrr <- (Zr*Zr) >> mandel_shift
+        MV      a0, s4
+        MV      a1, s5
+        CALL    mulsi3
+        SRLI    s7, a0, mandel_shift_m1 ; Zri <- (Zr*Zi) >> (mandelshift-1)
+        MV      a0, s5
+        MV      a1, s5
+        CALL    mulsi3
+        SRLI    s8, a0, mandel_shift    ; Zii <- (Zi*Zi) >> mandelshift
+        SUB     s4, s6, s8              ; Zr <- Zrr - Zii + Cr
+        ADD     s4, s4, s2
+        ADD     s5, s7, s3              ; Zi <- 2*Zri + cr
+
+        ADD     s6, s6, s8              ; Exit, if norm > norm_max
+        LI      s7, norm_max
+        BGT     s6, s7, exit_z
+
+        ADDI    s10, s10, -1            ; iter--
+        BNEZ    s10, loop_z
+
+
+        exit_z:
+        LI      a0, colormap            ; choose color
+        ADD     a0, a0, s10
+        LBU     a0, a0, 0
         CALL    putc
 
-        ADDI    s0, s0, 1       ; Increase x-counter
+        ADDI    s0, s0, 1               ; Increase x-counter
         ADDI    s2, s2, dx
         BNE     s0, s11, loop_x
 
-        LI      a0, 13          ; Send line ending
+        LI      a0, 13                  ; Send line ending
         CALL    putc
         LI      a0, 10
         CALL    putc
 
-        ADDI    s1, s1, 1       ; Increase y-counter
+        ADDI    s1, s1, 1               ; Increase y-counter
         ADDI    s3, s3, dy
         BNE     s1, s11, loop_y
 
+        J       mandelstart
+
         EBREAK
+
+        wait:
+        LI      t0, 1
+        SLLI    t0, t0, slow_bit
+        wait_loop:
+        ADDI    t0, t0, -1
+        BNEZ    t0, wait_loop
+        RET
 
         mulsi3:                 ; integer multiplication
         MV      a2, a0
@@ -82,6 +138,7 @@ class Mem(Elaboratable):
         AND     t1, t1, t0      ; Test
         BNEZ    t1, putc_loop
         RET
+
         """)
 
         a.assemble()
